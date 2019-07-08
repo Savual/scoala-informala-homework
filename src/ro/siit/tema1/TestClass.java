aa203b5c56e9c5c1f6b972b47e37a6641ed0f5b9
Savual <savual@yahoo.com>
2019-07-08 20:08:32 +0300

    Very important update...

--- src/ro/siit/tema1/TestClass.java
+++ src/ro/siit/tema1/TestClass.java
@@ -3,5 +3,6 @@ package ro.siit.tema1;
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("De cate ori deschid fereastra de atatea ori o inchid");
+        System.out.println("test");
     }
 }
